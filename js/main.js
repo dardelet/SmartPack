7e7af88a49279c2acabb48c15ca820664ce73b8c
Michael <mollard@eurecom.fr>
2015-01-12 17:12:24 +0100

    Controller added and some modification of the html. Test to be done

--- js/main.js
+++ js/main.js
@@ -85,6 +85,50 @@ angular.module('app',[])
     }
   };
 
+}])
+.controller('categoryListController',['$scope',function($scope) {
+    $scope.categories = [
+	{
+	    id:1,
+	    objects:[
+		{
+		    name:"Lol1"
+		    imgsrc:""
+		},
+		{
+		    name:"Lol2"
+		    imgsrc:""
+		},
+		{
+		    name:"Lol3"
+		    imgsrc:""
+		}
+	    ]
+	},
+	{
+	    id:2,
+	    objects:[]
+	},
+	    id:3,
+	    objects:[]
+	}
+    ];
+
+    $scope.addCategory = function () {
+	var category = nex Object();
+	category.id = $scope.categories.length + 1;
+	category.objects = [];
+	$scope.categories.push(category);
+    }
+
+    $scope.handleObject = function (name) {
+	if (typeof $scope[name] !== "undefined"){
+	    $scope[name] = !$scope[name];
+	} else {
+	    $scope[name]=true;
+	}
+    }
+
 }]);
 
 //Init the search box
