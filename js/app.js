468e6c2de34e0f049867ce7e2903b0bcef72383c
Michael Mollard <mollard@catria.eurecom.fr>
2015-01-15 13:36:38 +0100

    Connection initialized with localStorage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ var app = angular.module('app', ['EventEmitterService','ngAnimate']);
 app.controller('mainController', ['$scope','$ev', function($scope,$ev) {
     $scope.modalShown = false;
     $scope.logged=false;
+    if(window.localStorage){
+        if(localStorage.logged ==="true"){
+            $scope.logged=true;
+        }
+    }
     $scope.name="User";
     $scope.toggleModal = function() {
         $scope.modalShown = !$scope.modalShown;
@@ -9,15 +14,15 @@ app.controller('mainController', ['$scope','$ev', function($scope,$ev) {
     $scope.disconnect = function(){
     	$scope.logged=false;
         if(window.localStorage){
-            localStorage.logged = false;
+            localStorage.logged = "false";
         }
     	$ev.emit('logged:false', false);
     };
     $ev.on('logged:true', function (value) {
             $scope.logged = true;
             if(window.localStorage){
-                localStorage.logged = true;
+                localStorage.logged = "true";
             }
-	    scope.modalShown = !$scope.modalShown;
+	    $scope.modalShown = !$scope.modalShown;
     });
 }])
