38f15262cd1aad356427163ff1b726e99827caf8
Radhwane <radcheb@gmail.com>
2015-01-15 12:49:46 -0800

    adding team

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var screen_height = $(window).height();
+
 $('section').css('min-height', screen_height + 'px');
+var message_newHeight=(parseInt($('#row1').css('height'),10) -15)+"px";
+$('#message2').css('min-height',message_newHeight);
+
 $(window).on('resize', function() {
     var win = $(this); //this = window
     $('section').css('min-height', win.height + 'px');
+    var message_newHeight=(parseInt($('#row1').css('height'),10) -15)+"px";
+    $('#message2').css('min-height',message_newHeight);
 });
+
 var language = 'fr-FR';
 // setting up the data picker
 $('#departure_date').datepicker({
